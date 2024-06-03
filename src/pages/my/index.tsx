ae85cab92891f875ddacc151aaf6817c90af830d
YuYueMoLiking <158788166+YuYueMoLiking@users.noreply.github.com>
2024-06-04 00:11:55 +0800

    feat: 简历优势

--- src/pages/my/index.tsx
+++ src/pages/my/index.tsx
@@ -7,6 +7,8 @@ const MyPage = () => {
           <h2>基础信息</h2>
           <ul>
             <li>姓名：汪冉洋(男)</li>
+            <li>2+年工作经验</li>
+            <li>中高端前端开发工程师</li>
             <li>手机号：13937627224</li>
             <li>学历：本科(全日制)</li>
             <li>专业：计算机科学与技术</li>
@@ -16,28 +18,42 @@ const MyPage = () => {
         <div className="baseInfo userAdvantages">
           <h2>个人优势</h2>
           <ul className="numList" >
-            <li>开发框架：精通React开发框架，熟练使用Vue开发框架；</li>
-            <li>项目框架：熟练使用Umi Max和Taro框架；</li>
-            <li>开发组件：精通Ant Design，ProComponents，TaroUI等常用组件库；</li>
-            <li>开发语言：熟练使用JavaScript编程语言，包括DOM，语法函数；</li>
-            <li>开发配合：精通云效，Apipost，蓝湖，飞书以及figma进行项目和其他开发人员配合；</li>
-            <li>开发基础：熟练掌握HTML，CSS知识用于网页构建以及样式布局；</li>
-            <li>熟练使用typescript进行类型处理；</li>
-            <li>熟练使用高德API进行车辆轨迹绘制；</li>
-            <li>熟练使用TaroUI进行微信小程序的开发；</li>
-            <li>熟悉通过二次封装的Axios来和后端接口进行数据交互；</li>
-            <li>了解浏览器兼容性问题，可以编写出兼容浏览器的代码</li>
+            <li>精通 HTML5 和 CSS3 的布局与样式；</li>
+            <li>掌握 JavaScript（ES6+）语法，熟练运用；</li>
+            <li>熟练使用 React 框架，掌握 Vue 框架；</li>
+            <li>熟悉 Ant Design、TaroUI 和 Element 等组件库；</li>
+            <li>熟悉 Umi Max、Taro 等前端应用开发框架；</li>
+            <li>掌握 Webpack、Vite 等构建工具的配置与使用；</li>
+            <li>熟练运用 Git 进行团队协作；</li>
+            <li>了解 Unix 命令；</li>
+            <li>了解前端项目发布与部署；</li>
+            <li>了解微前端框架 Qiankun；</li>
+            <li>在小程序和移动端项目开发有经验；</li>
+            <li>了解浏览器兼容性问题，能编写兼容性代码；</li>
+            <li>了解 Python 和 Android 开发。</li>
+            A
           </ul>
         </div>
         <div className="baseInfo workExperience">
           <h2>工作经历</h2>
           <div className="workOne">
             <div className="companyName">
-              <div>
-                <h3>南京智能汇科技有限公司</h3>
+              <div className="companyNameBox">
+                <h3>南京信息有限公司</h3>
                 <p>前端开发工程师</p>
               </div>
-              <div>2023.10-至今</div>
+              <div className='companyTime' >2023.10-至今</div>
+            </div>
+            {/*工作内容*/}
+            <div className='workContent'>
+              <p>工作内容</p>
+              <ul>
+                <li>熟练使用 React 全家桶，高效开发业务且代码可读性维护性高；</li>
+                <li>封装项目中高频出现组件；</li>
+                <li>封装项目中常用数据结构处理函数;</li>
+                <li>利用MockJs等工具提升项目开发效率；</li>
+                <li>使用Husky、Eslint、Pritter等工具规范项目代码；</li>
+              </ul>
             </div>
           </div>
         </div>
